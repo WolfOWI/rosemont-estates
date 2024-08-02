55fb574f3814f07f7941f2cc24bede75068b4fc2
WolfOWI <97434628+WolfOWI@users.noreply.github.com>
2024-08-02 09:05:12 +0200

    Add Google Material Icons

--- src/pages/LogInPage.jsx
+++ src/pages/LogInPage.jsx
@@ -1,5 +1,12 @@
+import { BedOutlined } from "@mui/icons-material";
+
 function LogInPage() {
-  return <div className="text-green-700">Log In Page</div>;
+  return (
+    <>
+      <div className="text-green-700">Log In Page</div>
+      <BedOutlined className="" />
+    </>
+  );
 }
 
 export default LogInPage;
